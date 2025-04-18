92b527e18662c60e277d71f2820dd046f07c995e
ultimatekristency <ultimatekristency@gmail.com>
2025-04-18 22:38:52 +0100

    first commit

---
+++ client/target/debug/deps/client-b50202a84b8f9f4b.d
@@ -0,0 +1,5 @@
+C:\Users\THINKPAD T490s\personal_project\Zynthrak\client\target\debug\deps\libclient-b50202a84b8f9f4b.rmeta: src/main.rs
+
+C:\Users\THINKPAD T490s\personal_project\Zynthrak\client\target\debug\deps\client-b50202a84b8f9f4b.d: src/main.rs
+
+src/main.rs:
---
+++ client/target/debug/deps/client-efb00a6dac0eda1c.d
@@ -0,0 +1,5 @@
+C:\Users\THINKPAD T490s\personal_project\Zynthrak\client\target\debug\deps\libclient-efb00a6dac0eda1c.rmeta: src/main.rs
+
+C:\Users\THINKPAD T490s\personal_project\Zynthrak\client\target\debug\deps\client-efb00a6dac0eda1c.d: src/main.rs
+
+src/main.rs:
